b38dbff56153a518b7eda31280f8d289998fc0d8
Benjamin Schreiber <bpschreiber2003@gmail.com>
2023-04-21 16:38:31 -0700

    fixed merge

--- PA 9/main.cpp
+++ PA 9/main.cpp
@@ -1,23 +1,17 @@
 
 #include "raylib.h"
 #include "const.h"
+#include "Entity.hpp"
+#include <vector>
 
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-
-
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - keyboard input");
-
     std::vector<Entity*> entities = {};
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
 
-    // Main game loop
     while (!WindowShouldClose())   
     {
 
@@ -26,8 +20,6 @@ int main(void)
             e->handleCollision(nullptr);
         }
 
-        // Draw
-        //----------------------------------------------------------------------------------
         BeginDrawing();
 
         for (const auto& e : entities) {
