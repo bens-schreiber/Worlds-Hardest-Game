17caebd29765c0798f7523b2356ff1e968c8b880
kendalllyons <kendalllyons@pinnacle-pt.com>
2023-04-21 16:24:21 -0700

    Delete comments pt.2 bc ben was mad i left AWSD

--- PA 9/main.cpp
+++ PA 9/main.cpp
@@ -7,8 +7,6 @@ int main(void)
     const int screenWidth = 1600;
     const int screenHeight = 900;
 
-    Vector2 ballPosition = { (float)screenWidth / 5, (float)screenHeight / 5 };
-
     InitWindow(screenWidth, screenHeight, "World's Hardest Game");
 
     SetTargetFPS(60);            
@@ -18,12 +16,6 @@ int main(void)
     while (!WindowShouldClose())   
     {
 
-        if (IsKeyDown(KEY_D)) ballPosition.x += 4.0f;
-        if (IsKeyDown(KEY_A)) ballPosition.x -= 4.0f;
-        if (IsKeyDown(KEY_W)) ballPosition.y -= 4.0f;
-        if (IsKeyDown(KEY_S)) ballPosition.y += 4.0f;
-
-    
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
