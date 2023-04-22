c43092d55a4187074e8a3381059f34e66b4b1e9d
Drewskiii <andrewvarkey24@gmail.com>
2023-04-21 17:16:40 -0700

    Entity Fix

--- PA 9/Entity.hpp
+++ PA 9/Entity.hpp
@@ -4,12 +4,12 @@ class Entity {
 protected:
 	Vector2 m_position;
 	Vector2 m_velocity;
-	Vector2 m_accelration;
+	Vector2 m_acceleration;
 
 	Entity(
-		Vector2 m_position = { 0,0 }, 
-		Vector2 m_velocity = { 0,0 }, 
-		Vector2 m_acceleration = { 0,0 }) 
+		Vector2 position = { 0,0 },
+		Vector2 velocity = { 0,0 },
+		Vector2 acceleration = { 0,0 }) : m_position(position), m_velocity(velocity), m_acceleration(acceleration) 
 	{}
 
 public:
