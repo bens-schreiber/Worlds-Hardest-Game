d98e9a3e474c39f73ff75c897190d91d48548e60
Benjamin Schreiber <bpschreiber2003@gmail.com>
2023-04-22 12:35:08 -0700

    .gitignore now works

--- PA 9/PlayerCollidable.hpp
+++ PA 9/PlayerCollidable.hpp
@@ -1,10 +1,14 @@
 #pragma once
 #include "Player.hpp"
 
+// Interface for dealing with player collisions
+// Any class that implements PlayerCollidable will be able to collide with the player, and
+// edit the players movement
 class PlayerCollidable {
 public:
 	PlayerCollidable(Player* player) : m_player(player) {}
 
+	// Collide with player logic
 	virtual void handleCollision() = 0;
 
 protected:
