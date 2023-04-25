bec4d10518cd39fc05f14850e4db0fe6a22a03c4
kendalllyons <kendalllyons@pinnacle-pt.com>
2023-04-25 16:26:41 -0700

    Updated class (circleSpeed variable)

--- PA 9/TrigBall.h
+++ PA 9/TrigBall.h
@@ -12,14 +12,17 @@ class TrigBall : public Entity, public MapCollidable, PlayerCollidable
 
 	//Vector2 m_boundsX;	// x lower, y higher
 	//Vector2 m_boundsY;	// x lower, y higher
-	int circleRadius; // size of area you want to circle
 
 public:
 
 	TrigBall(Vector2 position,
 		Vector2 velocity, 
-		int circleRadius)  // size of area you want to circle
-		: Entity(position, velocity) {};
+		int circleRadius,
+		int circleSpeed)  // size of area you want to circle
+		: Entity(position, velocity) {
+		this->m_circleRadius = circleRadius;
+		this->m_cirlceSpeed = circleSpeed;
+	};
 
 	void draw() {
 
@@ -30,8 +33,8 @@ public:
 		DrawCircle(m_position.x, m_position.y, ballRadius - 7, DARKBLUE);
 	}
 	void update() {
-		m_position.x = m_position.x + circleRadius * (float)cos(0.05 * GetTime() * PI);
-		m_position.y = m_position.y + circleRadius * (float)cos(0.05 * GetTime() * PI);
+		m_position.x = m_position.x + m_circleRadius * (float)cos(m_cirlceSpeed * GetTime() * PI);
+		m_position.y = m_position.y + m_circleRadius * (float)cos(m_cirlceSpeed * GetTime() * PI);
 		handlePlayerCollision();
 	}
 	void handlePlayerCollision(Rectangle rect = {}) {
@@ -47,6 +50,10 @@ public:
 	bool checkMapCollision(Rectangle rect = {}) {
 		return CheckCollisionCircleRec({ m_position.x, m_position.y }, 0, rect);
 	}
+
+private:
+	int m_circleRadius; // size of area you want to circle
+	int m_cirlceSpeed; // somewhere around 0.05 is reasonable
 
 	/*void handleMapOutOfBounds() {
 		flipVelocityX();
